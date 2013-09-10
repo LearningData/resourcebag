a99dbc657e4dd6f5f24cd73a131558aeaf20b183
Edgar <edgar@learningdata.ie>
2013-09-10 08:11:45 +0000

    change cache directory and your permission. Add gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -14,7 +14,7 @@ return new \Phalcon\Config(array(
         'viewsDir'       => __DIR__ . '/../../app/views/',
         'pluginsDir'     => __DIR__ . '/../../app/plugins/',
         'libraryDir'     => __DIR__ . '/../../app/library/',
-        'cacheDir'       => '/tmp/cache/',
+        'cacheDir'       => __DIR__ . '/../../app/cache/',
         'baseUri'        => '/schoolbag/',
     )
 ));
