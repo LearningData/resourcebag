04c59538e34aa40a03228b0db0e722453dcf6122
Edgar <edgar@learningdata.ie>
2013-10-08 09:24:58 +0100

    created download controller
    this controller will be used for all downloads
    removed download of homework controller

--- app/controllers/HomeworkController.php
+++ app/controllers/HomeworkController.php
@@ -74,20 +74,6 @@ class HomeworkController extends ControllerBase {
         return $this->response->redirect($uri);
     }
 
-    public function downloadFileAction($fileId) {
-        $file = HomeworkFile::findFirst("id = $fileId");
-        if (!$file) {
-            $this->flash->error("Error to download file.");
-        }
-
-        header("Content-length: " . $file->size);
-        header("Content-type: " . $file->type);
-        header("Content-Disposition: attachment; filename=" . $file->name);
-
-        $this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_NO_RENDER);
-        $this->response->setContent($file->file)->send();
-    }
-
     public function submitAction($homeworkId) {
         $homework = Homework::findFirstById($homeworkId);
         $this->getUserBySession();
