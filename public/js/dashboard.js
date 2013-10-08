a437d28c33136054dc3d68ba0d4a1577383bf90b
james <james@rameau.(none)>
2013-10-08 08:44:01 +0100

    start dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,28 @@
+var dashboard = (function() {
+
+    var urlBase = "http://localhost:7001/schoolbag"
+    var studentId = 1652
+
+    init = function() {
+        populateHomework()
+    
+    }
+
+    populateHomework = function() {
+        var url = urlBase + "/service/homeworks/" + studentId;
+        $.get(url, function(response) {
+            var works = []
+            for ( var i = 0; i < response.homeworks.length; i++ ) {
+                works.push("<a href=" + urlBase + "/student/homework/edit/" + -response.homeworks[i].id + "><li><span class=\"checkbox-homework\"></span><p>" + response.homeworks[i].description + " (" + response.homeworks[i].subject + ")</p></li></a>")
+            }
+            var homeworkList = $( "<ul class=\"homeworkList\">")
+            homeworkList.append( works.join("") )
+            $( "#dashboard-homework" ).append( homeworkList )
+        })
+    }
+
+    return {
+        init: init
+    };
+})()
+
