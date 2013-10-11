5db68240e285a76b2da9288c552517ad422650a1
Edgar <edgar@learningdata.ie>
2013-10-11 08:54:27 +0100

    index template doesnt use default template anymore and add migration for upload photo

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,6 +1,9 @@
 <?php
+use Phalcon\Mvc\View;
 
 class IndexController extends Phalcon\Mvc\Controller {
-    public function indexAction() {}
+    public function indexAction() {
+        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+    }
 }
 
