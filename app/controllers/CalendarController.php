c6d902a9e4c4f8a08e3ed5a6d6e44edd423efbf6
Edgar <edgar@learningdata.ie>
2013-10-14 12:59:50 +0100

    Now calendar is working for teacher and student

--- app/controllers/CalendarController.php
+++ app/controllers/CalendarController.php
@@ -9,6 +9,10 @@ class CalendarController extends ControllerBase {
         $this->getUserBySession();
     }
 
+    public function indexAction() {
+        $this->getUserBySession();
+    }
+
     public function createAction() {
         $user = $this->getUserBySession();
         $event = new Event();
