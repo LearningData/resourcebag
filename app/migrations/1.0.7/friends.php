0a8e8c13f0211b8ae0d584b1fb112fd4015a05f3
Edgar <edgar@learningdata.ie>
2013-10-14 12:44:55 +0100

    created action to create an event and show this event on calendar

---
+++ app/migrations/1.0.7/friends.php
@@ -0,0 +1,54 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class FriendsMigration_107 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'friends',
+            array(
+            'columns' => array(
+                new Column(
+                    'TeacherFrom',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'TeacherTo',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'TeacherFrom'
+                    )
+                ),
+                new Column(
+                    'Verified',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'TeacherTo'
+                    )
+                )
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'latin1_swedish_ci'
+            )
+        )
+        );
+    }
+}
--- app/views/student/calendar/index.volt
+++ app/views/student/calendar/index.volt
@@ -1,2 +1,3 @@
 <h1>Calendar</h1>
+<h3>{{ link_to("calendar/new", "New Event") }}</h3>
 <div id="calendar"></div>
