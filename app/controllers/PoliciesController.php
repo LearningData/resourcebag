0e2101313137a74d45d86e6e3904d6615289d072
Edgar <edgar@learningdata.ie>
2013-10-16 17:17:13 +0100

    added action to download policies school and upload

--- app/controllers/PoliciesController.php
+++ app/controllers/PoliciesController.php
@@ -1,9 +1,40 @@
 <?php
+require "../app/services/FileService.php";
+use \Phalcon\Config\Adapter\Ini as Config;
 
 class PoliciesController extends ControllerBase {
     public function indexAction() {
-
+        $this->getUserBySession();
+        $this->view->files = FileService::listFiles($this->getDir());
     }
 
+    public function newAction() {}
+
+    public function uploadAction() {
+        if ($this->request->hasFiles() == true) {
+            foreach ($this->request->getUploadedFiles() as $file) {
+                move_uploaded_file($file->getTempName(),
+                    $this->getDir() . $file->getName());
+            }
+
+        }
+
+        return $this->response->redirect("policies");
+    }
+
+    public function downloadAction($fileName) {
+        $this->view->setRenderLevel(\Phalcon\Mvc\View::LEVEL_NO_RENDER);
+        header('Content-Disposition: attachment; filename="'.$fileName.'"');
+        readfile($this->getDir() . $fileName);
+    }
+
+    private function getDir() {
+        $config = new Config("../app/config/files-config.ini");
+        $user = $this->getUserBySession();
+        $dir = $config->files->dir . $user->schoolId . "/";
+
+        return $dir;
+    }
 }
 
+?>
--- app/views/policies/index.volt
+++ app/views/policies/index.volt
@@ -1,1 +1,7 @@
 <h1>School Policies</h1>
+{% if user.isSchool() %}
+    {{ link_to("policies/new", "New")}}
+{% endif %}
+{% for file in files %}
+    <p>{{ link_to("policies/download/"~file, file)}}</p>
+{% endfor %}
