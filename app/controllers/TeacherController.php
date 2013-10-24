171ca4dfbbc88e02b0bd07776c0c77f5130a4750
Edgar <edgar@learningdata.ie>
2013-10-24 15:07:14 +0100

    just teacher can access teacher controller

--- app/controllers/TeacherController.php
+++ app/controllers/TeacherController.php
@@ -2,6 +2,14 @@
 require "../app/services/Timetable.php";
 
 class TeacherController extends UsersController {
+    public function beforeExecuteRoute($dispatcher){
+        $user = Authenticate::getUser();
+
+        if(!$user->isTeacher()) {
+            return $this->response->redirect("dashboard");
+        }
+    }
+
     public function listTeachersAction() {
         $teachers = $this->view->user->getTeachers();
         $this->view->teachers = $teachers;
