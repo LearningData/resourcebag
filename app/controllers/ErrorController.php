53cfd1553696c518a1d8e233fb62fe42d2605c99
Edgar <edgar@learningdata.ie>
2013-11-05 16:44:45 +0000

    error controller using translation

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -4,6 +4,7 @@ use Phalcon\Mvc\View;
 class ErrorController extends \Phalcon\Mvc\Controller {
     public function notFoundAction() {
         $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+        $this->view->t = Translation::get(Language::get(), "error");
     }
 }
 
