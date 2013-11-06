1cd8f18cdc275910631c0eb514b4f45b1622a644
james <james@rameau.(none)>
2013-11-06 11:07:40 +0000

    add notices translation file

---
+++ app/lang/en/notices.php
@@ -0,0 +1,17 @@
+<?php
+$messages = array(
+    "add file" => "Add File",
+    "all teachers" => "All Teachers"
+    "create notice for" => "Create Notice For",
+    "message" => "Message",
+    "new notice" => "New Notice",
+    "notice category" => "Notice Category"
+    "notice date" => "Notice Date",
+    "restrict to" => "Restrict to",
+    "teachers and students" => "Teachers and Students",
+    "teachers only" => "Teachers only"
+);
+?>
+
+
+    
