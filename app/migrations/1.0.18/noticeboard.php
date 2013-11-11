e58dabe7b315e6852920183ede1b86109b8ecbf6
Edgar <edgar@learningdata.ie>
2013-11-11 16:30:38 +0000

    migration to change the field varchar text of noticeboard to text.

---
+++ app/migrations/1.0.18/noticeboard.php
@@ -0,0 +1,117 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class NoticeboardMigration_1018 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'noticeboard',
+            array(
+            'columns' => array(
+                new Column(
+                    'schoolID',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 5,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'date',
+                    array(
+                        'type' => Column::TYPE_DATETIME,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'schoolID'
+                    )
+                ),
+                new Column(
+                    'text',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 255,
+                        'after' => 'date'
+                    )
+                ),
+                new Column(
+                    'userType',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 1,
+                        'after' => 'text'
+                    )
+                ),
+                new Column(
+                    'uploadedBy',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 8,
+                        'after' => 'userType'
+                    )
+                ),
+                new Column(
+                    'classID',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'size' => 11,
+                        'after' => 'uploadedBy'
+                    )
+                ),
+                new Column(
+                    'fileAttached',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 100,
+                        'after' => 'classID'
+                    )
+                ),
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 9,
+                        'after' => 'fileAttached'
+                    )
+                ),
+                new Column(
+                    'title',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 100,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'category',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'size' => 50,
+                        'after' => 'title'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id'))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '246',
+                'ENGINE' => 'MyISAM',
+                'TABLE_COLLATION' => 'latin1_swedish_ci'
+            )
+        )
+        );
+    }
+}
