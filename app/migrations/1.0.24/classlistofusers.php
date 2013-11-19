6eadc7ca4aa549ff0f1cf85959e06cc0d4622ac3
Edgar <edgar@learningdata.ie>
2013-11-19 16:00:00 +0000

    created migration to add id to table timetableslot

---
+++ app/migrations/1.0.24/classlistofusers.php
@@ -0,0 +1,54 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class ClasslistofusersMigration_1024 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'classlistofusers',
+            array(
+            'columns' => array(
+                new Column(
+                    'schoolID',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 5,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'classID',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 5,
+                        'after' => 'schoolID'
+                    )
+                ),
+                new Column(
+                    'studentID',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'notNull' => true,
+                        'size' => 11,
+                        'after' => 'classID'
+                    )
+                )
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '',
+                'ENGINE' => 'MyISAM',
+                'TABLE_COLLATION' => 'latin1_swedish_ci'
+            )
+        )
+        );
+    }
+}
