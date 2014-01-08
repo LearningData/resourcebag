8de8f8153e447a542488119fabc699c273b09f68
Edgar <edgar@learningdata.ie>
2014-01-08 16:18:10 +0000

    changed the repo url and the project folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 set :application, 'file-server'
-#set :repo_url, 'git@git.learningdata.net:/srv/d_jess01/git/repositories/resourcebag.git'
-set :repo_url, "https://github.com/edgarlatorre/website.git"
+set :repo_url, 'git@git.learningdata.net:/srv/d_jess01/git/repositories/resourcebag.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, '/Projects/file-server'
@@ -17,28 +16,28 @@ namespace :deploy do
   desc "Start application"
   task :start do
     on roles(:app) do
-      execute "cd #{deploy_to} && forever start app.js"
+      execute "cd #{deploy_to}/current && forever start app.js"
     end
   end
 
   desc "Stop application"
   task :stop do
     on roles(:app) do
-      execute "cd #{deploy_to} && forever stop app.js"
+      execute "cd #{deploy_to}/current && forever stop app.js"
     end
   end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "cd #{deploy_to} && forever restart app.js"
+      execute "cd #{deploy_to}/current && forever restart app.js"
     end
   end
 
   desc 'Npm install'
   task :npm_install do
     on roles(:app) do
-      execute "cd /Projects/file-server && npm install"
+      execute "cd #{deploy_to} && npm install"
     end
   end
 
