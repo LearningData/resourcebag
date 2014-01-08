fd5cf06004b6ee4bcbf076a8df5376f3f00d165b
Edgar <edgar@learningdata.ie>
2014-01-08 15:36:31 +0000

    added capistrano to automate the deploy

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+set :application, 'file-server'
+#set :repo_url, 'git@git.learningdata.net:/srv/d_jess01/git/repositories/resourcebag.git'
+set :repo_url, "https://github.com/edgarlatorre/website.git"
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application_path, "/Projects/file-server"
+set :deploy_to, '/Projects/file-server'
+set :deploy_via, :remote_cache
+set :scm, :git
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+  desc "Start application"
+  task :start do
+    on roles(:app) do
+      execute "cd #{deploy_to} && forever start app.js"
+    end
+  end
+
+  desc "Stop application"
+  task :stop do
+    on roles(:app) do
+      execute "cd #{deploy_to} && forever stop app.js"
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "cd #{deploy_to} && forever restart app.js"
+    end
+  end
+
+  desc 'Npm install'
+  task :npm_install do
+    on roles(:app) do
+      execute "cd /Projects/file-server && npm install"
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+end
