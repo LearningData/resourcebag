9b1e78774c43c7789b653e277e9cae0375190d4b
Edgar <edgar@learningdata.ie>
2014-01-14 09:54:42 +0000

    add configuration to staging server

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,14 @@ set :repo_url, 'git@git.learningdata.net:/srv/d_jess01/git/repositories/resource
 # set :repo_url, 'file:///Users/edgar/Projects/file-server/.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :deploy_to, '/Projects/file-server'
-set :deploy_via, :remote_cache
+set :deploy_to, '/home/azureuser/resourcebag'
+set :deploy_via, :copy
 set :scm, :git
+set :scm_password, "LD46marmita"
+set :scm_passphrase, "LD46marmita"
 set :use_sudo, false
 set :pty, true
+set :ssh_options, {:forward_agent => true}
 # set :format, :pretty
 # set :log_level, :debug
 # set :scm_verbose, true
@@ -18,28 +21,28 @@ set :pty, true
 namespace :deploy do
   desc "Start application"
   task :start do
-    on roles(:app) do
+    on roles(:web) do
       execute "cd #{deploy_to} && forever start app.js"
     end
   end
 
   desc "Stop application"
   task :stop do
-    on roles(:app) do
+    on roles(:web) do
       execute "cd #{deploy_to} && forever stop app.js"
     end
   end
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:web), in: :sequence, wait: 5 do
       execute "cd #{deploy_to} && forever restart app.js"
     end
   end
 
   desc 'Npm install'
   task :npm_install do
-    on roles(:app) do
+    on roles(:web) do
       execute "cd #{deploy_to} && npm install"
     end
   end
